3bab57771d8dc73f91b44db0784f165e26bb2a45
lgonzalez <luisfgonzalez91@gmail.com>
2023-07-09 23:46:54 -0500

    fixed typo

--- storage/create_tables.sql
+++ storage/create_tables.sql
@@ -27,7 +27,7 @@ create table if not exists Products(
 create table if not exists Users(
 	Username varchar(255) primary key,
 	Password varchar(255) not null,
-	FirsName varchar(255),
+	FirstName varchar(255),
 	LastName varchar(255),
 	Email varchar(255),
 	CreatedAt timestamp default current_timestamp
